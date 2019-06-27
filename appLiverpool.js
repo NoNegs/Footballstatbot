52997d282778b51bf8286a2adf86b87e4b90e334
NoNegs <carlosmomo@hotmail.co.uk>
2019-06-27 11:24:58 +0100

    Working code for Footballstatbot

--- appLiverpool.js
+++ appLiverpool.js
@@ -3,17 +3,50 @@ const app = express()
 const morgan = require('morgan')
 const mysql = require('mysql')
 
+const bodyParser = require ('body-parser')
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(express.static("./public"))
+
 app.use(morgan('combined'))
+
+app.post('/suggestions_create', (req, res) => {
+    console.log("Trying to store new suggestion")
+
+    console.log("First Name: " + req.body.create_first_name)
+    const firstName = req.body.create_first_name
+    const secondName = req.body.create_second_name
+    const suggestions = req.body.create_suggestions
+
+
+    const queryString = "INSERT INTO suggestions (first_name, second_name, suggestions) VALUES (?, ?, ?)"
+    getConnection().query(queryString, [firstName, secondName, suggestions], (err, results, fields) => {
+        if (err) {
+            console.log("Failed to insert new user: " + err)
+            res.sendStatus(500)
+            return
+        }
+
+        console.log("Inserted new suggestion")
+        res.end()
+    })
+
+})
+
+function getConnection () {
+    return mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'nationwide'
+})
+}
 
 app.get('/liverpool/:month', (req, res) => {
     console.log("Fetching goals scored in the Month of " + req.params.month)
    
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'nationwide'
-    })
+    const connection = getConnection()
     
     const gMonth = req.params.month
     const queryString = "SELECT * FROM Liverpool WHERE Month = ?"
@@ -24,22 +57,31 @@ app.get('/liverpool/:month', (req, res) => {
             return
         }
         console.log("I think goals were fetched successfully")
-        res.json(rows)
+       res.json(rows)
     })
 
 })
 
-app.get("/", (req, res) => {
-    console.log("Responding to root route")
-    res.send("Hello from ROOOT")
-})
+app.get('/liverpool', (req, res) => {
+   
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'nationwide'
+    })
+    
+    const queryString = "SELECT * FROM Liverpool"
+    connection.query(queryString, (err, rows, fields) => {
+        if (err) {
+            console.log("Failed to query goals: " + err)
+            res.sendStatus(500)
+            return
+        }
+        res.json(rows)
+    })
 
-app.get("/users", (req, res) => {
-   const user1 = {firstName: "Stephen", lastName: "Curry"}
-   const user2 = {firstName: "Kevin", lastName: "Durrant"}
-   res.json([user1, user2])
 })
-
 
 app.listen(3003, () => {
     console.log("Server is up and listening on 3003...")
